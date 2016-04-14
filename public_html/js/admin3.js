eb9c02418a67dd259bbcb5049beceb40ba313084
celt1323 <houseoftan4@gmail.com>
2016-04-14 08:05:02 -0700

    Added files via upload
    host  on web

--- public_html/js/admin3.js
+++ public_html/js/admin3.js
@@ -4,39 +4,39 @@ $(function () {
         VERSION = "v1";
 
     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
-    
-   
-    if(Backendless.UserService.isValidLogin){
-        userLoggedIn(Backendless.LocalCache.get("current-user-id"));
+    if(Backendless.UserService.isValidLogin()){
+         userLoggedIn(Backendless.LocalCache.get("current-user-id"));
     }else{
-    var loginScript = $("#login-template").html();
-    var loginTemplate = Handlebars.compile(loginScript);
-    $('.main-container').html(loginTemplate);
-}
-    
+          var loginScript = $("#login-template").html();
+          var loginTemplate = Handlebars.compile(loginScript);
+          $('.main-container').html(loginTemplate);
+    }
+
     $(document).on('submit', '.form-signin', function(event){
         event.preventDefault();
         
         var data = $(this).serializeArray(),
-        email = data[0].value,
-        password = data[1].value;
-        
+            email = data[0].value,
+            password = data[1].value;
+            
         Backendless.UserService.login(email, password, true, new Backendless.Async(userLoggedIn, gotError));
     });
-   
-   $(document).on('click', '.add-blog',function(){
-         var blogScript = $("#blog-template").html();
-    var blogTemplate = Handlebars.compile(blogScript);
     
-    $('.main-container').html(blogTemplate);
+    $(document).on('click', '.add-blog',function(){
+         var addBlogScript = $("#add-blog-template").html();
+    var addBlogTemplate = Handlebars.compile(addBlogScript);
+    
+    $('.main-container').html(addBlogTemplate);
    });
-   
    $(document).on('submit', '.form-add-blog', function(event){
+       if("empty"){
+        Materialize.toast('No title or content', 4000);   
+       }
        event.preventDefault();
-       
        var data = $(this).serializeArray(),
            title = data[0].value,
            content = data[1].value;
+
        
        var dataStore = Backendless.Persistence.of(Posts);
        
@@ -44,26 +44,23 @@ $(function () {
            title: title,
            content:  content,
            authorEmail: Backendless.UserService.getCurrentUser().email
-           
-       });
        
+       });
        dataStore.save(postObject);
        
        this.title.value = "";
        this.content.value = "";
-       
-       
    });
    
-   $(document).on('click', 'logout', function(){
+     $(document).on('click', '.logout', function(){
        Backendless.UserService.logout(new Backendless.Async(userLoggedOut, gotError));
        
         var loginScript = $("#login-template").html();
     var loginTemplate = Handlebars.compile(loginScript);
     $('.main-container').html(loginTemplate);
    });
-
-});
+    });
+    
 
 function Posts (args) {
     args = args || {};
@@ -80,9 +77,9 @@ function userLoggedIn(user){
     } else{
         userData = user;
     }
-    var welcomeScript = $('#welcome-template');
+    var welcomeScript = $('#welcome-template').html();
     var welcomeTemplate = Handlebars.compile(welcomeScript);
-    var welcomeHTML = welcomeTemplate(user);
+    var welcomeHTML = welcomeTemplate(userData);
     
     $('.main-container').html(welcomeHTML);
 }
@@ -92,13 +89,8 @@ function userLoggedOut(){
 }
 
 function gotError(error){
-    console.log("Error message - " + error.message);
-    console.log("Error code - " + error.message);
-    Materialize.toast('Incorrect username or password', 4000)
-}
-
-
-
-
-
+   console.log("Error message - " + error.message);
+    console.log("Error code - " + error.code);
+    Materialize.toast('Incorrect username or password', 4000);
+    }
 
